3b723f512ae6ee352710e63dbf59f49c0ee13cd1
Amogh Joshi <amoghj8@gmail.com>
2020-07-25 14:24:06 +0530

    BST impl

---
+++ Trees/BST.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* GetNewNode(int);
+Node* Insert(Node*, int);
+bool Search(Node*, int);
+
+int main(int argc, char const *argv[])
+{
+    int searchEle=-4;
+    Node* root = nullptr;
+    root = Insert(root, 5);
+    root = Insert(root, 15);
+    root = Insert(root, 0);
+    root = Insert(root, -4);
+    cout<< "Searching for: " << searchEle << endl;
+    string check = Search(root, searchEle) ? "yes" : "no";
+    cout<< searchEle << " present : " << check;
+    return 0;
+}
+
+Node* Insert(Node *root, int data) {
+    if(root == nullptr) {
+        root = GetNewNode(data);
+        return root;
+    }
+    else if(root->data>= data){
+        root->left = Insert(root->left, data);
+    }
+    else {
+        root->right = Insert(root->right, data);
+    }
+    return root;
+}
+
+Node* GetNewNode(int data){
+    Node *temp = new Node;
+    temp->data = data;
+    temp->left = nullptr;
+    temp->right = nullptr;
+    return temp;
+}
+
+bool Search(Node* root, int data) {
+    if(root==nullptr) {
+        return false;
+    }
+    if(root->data == data) {
+        return true;
+    }
+    if(root->data >= data ) {
+        return Search(root->left, data);
+    }
+    else {
+        return Search(root->right, data);
+    }
+}
